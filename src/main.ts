b4408bd72fc0dd17ab4585b49865d7c6c844f90f
JacksonVago <jackson@natividadesolucoes.com.br>
2026-07-13 14:53:42 -0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
+import * as path from 'path';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Caminho para os seus arquivos de certificado SSL
+  const keyPath = path.resolve(__dirname, '../ssl/server.key');
+  const certPath = path.resolve(__dirname, '../ssl/WWW_adminimovel_com_br.crt');
+
+  // Lê os arquivos de forma síncrona
+  const httpsOptions = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   const configService = app.get(EnvService);
   const port = configService.get('PORT');
